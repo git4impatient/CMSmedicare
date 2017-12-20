eb0118a849f293f852cfd3ff2e550538fea24b90
git4impatient <lurie@lurie.biz>
2017-12-20 17:55:06 -0500

    Update query2.sql

--- query2.sql
+++ query2.sql
@@ -1,7 +1,7 @@
 select g.physician_profile_id, 
-sum(g.total_amount_of_payment_usdollars) totpayments , count(*) 
+sum(g.total_amount_of_payment_usdollars) genpayments , count(*) 
 from generalpayments g 
 where g.total_amount_of_payment_usdollars is not null
 group by physician_profile_id
-order by totpayments desc
+order by genpayments desc
 limit 50;
