7d5643da19e1e4e96aee9e78486a19428de19b3f
git4impatient <lurie@lurie.biz>
2017-12-20 13:57:09 -0500

    Update goCMS.sh

--- goCMS.sh
+++ goCMS.sh
@@ -1,14 +1,17 @@
 wget http://download.cms.gov/openpayments/PGYR16_P063017.ZIP
-unzip PGYR16_P063017.ZIP
+jar -xvf  PGYR16_P063017.ZIP
 hadoop fs -rm -R /user/$USER/cms.db/ownership
 hadoop fs -rm -R /user/$USER/cms.db/generalpayments
 hadoop fs -rm -R /user/$USER/cms.db/researchpayments
+hadoop fs -mkdir /user/$USER/cms.db
 hadoop fs -mkdir /user/$USER/cms.db/ownership
 hadoop fs -mkdir /user/$USER/cms.db/researchpayments
 hadoop fs -mkdir /user/$USER/cms.db/generalpayments
-hadoop fs -put  OP_DTL_OWNRSHP_PGYR2016_P06302017.csv.sample /user/$USER/cms.db/ownership
-hadoop fs -put  OP_DTL_GNRL_PGYR2016_P06302017.csv.sample  /user/$USER/cms.db/generalpayments
-hadoop fs -put  OP_DTL_RSRCH_PGYR2016_P06302017.csv.sample /user/$USER/cms.db/researchpayments
-impala-shell -f generalpayments.ddl 
-impala-shell -f researchpayments.ddl 
-impala-shell -f ownership.ddl 
+hadoop fs -put  OP_DTL_OWNRSHP_PGYR2016_P06302017.csv /user/$USER/cms.db/ownership
+hadoop fs -put  OP_DTL_GNRL_PGYR2016_P06302017.csv  /user/$USER/cms.db/generalpayments
+hadoop fs -put  OP_DTL_RSRCH_PGYR2016_P06302017.csv /user/$USER/cms.db/researchpayments
+export DATANODE=$(sudo su - hdfs -c "hdfs dfsadmin -report | grep Hostname | sed 's/.*: //'  | tail -1")
+impala-shell -i $DATANODE -f cmsdb.ddl
+impala-shell -i $DATANODE -f generalpayments.ddl 
+impala-shell -i $DATANODE -f researchpayments.ddl 
+impala-shell -i $DATANODE -f ownership.ddl 
