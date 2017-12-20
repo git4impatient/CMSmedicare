3026e558beee01ff816d1bc2be5ffdb088561748
git4impatient <lurie@lurie.biz>
2017-12-20 17:54:36 -0500

    Update query1.sql

--- query1.sql
+++ query1.sql
@@ -1,6 +1,6 @@
 --select count(*) from (
 select g.physician_profile_id, 
-sum(g.total_amount_of_payment_usdollars) totpayments ,
+sum(g.total_amount_of_payment_usdollars) genpayments ,
 sum ( o.total_amount_invested_usdollars ) ownerinvest
 from generalpayments g left outer join ownership o on
   g.physician_profile_id =   o.physician_profile_id 
@@ -8,5 +8,5 @@ where total_amount_of_payment_usdollars is not null
 and  o.total_amount_invested_usdollars is not null
 
 group by physician_profile_id
-order by totpayments desc
+order by genpayments desc
 limit 50
