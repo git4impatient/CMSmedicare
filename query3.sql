57cc20e805dc892de44baee31eba61806fc9f66b
git4impatient <lurie@lurie.biz>
2017-12-20 17:46:47 -0500

    Create query3.sql

---
+++ query3.sql
@@ -0,0 +1,14 @@
+ select g.physician_profile_id, 
+sum(g.total_amount_of_payment_usdollars) totpayments ,
+sum(r.total_amount_of_payment_usdollars )researchpayments,
+sum ( o.total_amount_invested_usdollars ) ownerinvest
+from generalpayments g left outer join ownership o  on
+  g.physician_profile_id =   o.physician_profile_id 
+  left outer join researchpayments r on
+  g.physician_profile_id =   r.physician_profile_id
+where g.total_amount_of_payment_usdollars is not null
+--and  o.total_amount_invested_usdollars is not null
+
+group by physician_profile_id
+order by totpayments desc
+limit 50
