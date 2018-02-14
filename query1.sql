4c3ac33ef2be7dc603c414f46240696e250adf08
git4impatient <lurie@lurie.biz>
2018-02-13 21:13:58 -0500

    Update query1.sql

--- query1.sql
+++ query1.sql
@@ -9,7 +9,7 @@ sum ( o.total_amount_invested_usdollars ) ownerinvest
 from generalpayments g left outer join ownership o on
   g.physician_profile_id =   o.physician_profile_id 
 where total_amount_of_payment_usdollars is not null
-and  o.total_amount_invested_usdollars is not null
+--and  o.total_amount_invested_usdollars is not null
 
 group by physician_profile_id
 order by genpayments desc
