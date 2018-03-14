c51b0ef0137554bb0c83e413ed4c82a83dab542a
git4impatient <lurie@lurie.biz>
2018-03-13 20:15:15 -0400

    Create goLineItemDetail.sh

---
+++ goLineItemDetail.sh
@@ -0,0 +1,6 @@
+# Data from
+#https://www.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Medicare-Provider-Charge-Data/Physician-and-Other-Supplier2015.html
+
+https://data.cms.gov/use-agreement?id=sk9b-znav&name=Medicare%20Provider%20Utilization%20and%20Payment%20Data:%20Physician%20and%20Other%20Supplier%20PUF%20CY2015
+https://data.cms.gov/api/views/sk9b-znav/files/0c58ee11-92be-43ae-b7d5-b0a69d156ede?download=true&filename=C:\TemporaryWebFiles\Data%20Releases\Physician\2015\Medicare_Physician_and_Other_Supplier_PUF_Methodology_2017-06-15.pdf
+https://www.cms.gov/apps/ama/license.asp?file=http://download.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Medicare-Provider-Charge-Data/Downloads/Medicare_Provider_Util_Payment_PUF_CY2015.zip
